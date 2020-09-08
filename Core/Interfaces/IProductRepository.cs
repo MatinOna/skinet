b4cfcd0bca90e673d3e1940d4e1edebbb4593129
Martin Ona <mp.onajativa@gmail.com>
2020-09-08 11:46:38 -0500

    end section 3

---
+++ Core/Interfaces/IProductRepository.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using Core.Entities;
+
+namespace Core.Interfaces
+{
+    public interface IProductRepository
+    {
+        Task<Product> GetProductIdAsync(int id);
+        
+        Task<IReadOnlyList<Product>> GetProductAsync();
+                
+        Task<IReadOnlyList<ProductBrand>> GetProductBrandAsync();
+        Task<IReadOnlyList<ProductType>> GetProductTypeAsync();
+                
+    }
+}
